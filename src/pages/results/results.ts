99a1b30a89e014977674b28ea703c187997832a7
Nthabi <mputle15@gmail.com>
2018-11-02 10:16:24 +0200

    new changes

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -22,7 +22,9 @@ export class ResultsPage {
   items = this.navParams.get("items");
   recipes: any = [];
   arr = itemArr;
-  oops: string;
+  a: string;
+  b: string;
+  c: string;
   constructor(public navCtrl: NavController, public navParams: NavParams, private chefsFridge: ChefsfridgeProvider, public loadingCtrl: LoadingController) {
     console.log(this.category);
     console.log(this.sub);
@@ -30,11 +32,14 @@ export class ResultsPage {
     this.chefsFridge.itemSearch(this.category, this.sub, this.items).then((data)=>{
       this.recipes = data;
       console.log(this.recipes);
-
+      if (this.recipes == []) {
+        this.a = " Oops! Sorry...";
+        this.b = "There are no recipes with your ingredients";
+        this.c = "Please try again...";
+      }
+      
     })
-    // if (this.recipes == []) {
-    //   this.oops = " Oops! Sorry..."+<br>+"There are no recipes with your ingredients"+<br>"Please try again..."
-    // }
+    
   }
 
   ionViewDidLoad() {
